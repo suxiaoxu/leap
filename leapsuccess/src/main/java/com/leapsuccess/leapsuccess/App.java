9b319128bdc3692b5e9c8053c8fc45de78f5ced5
suxiaoxu <348586687@qq.com>
2017-03-04 11:44:39 +0800

    xiugai

---
+++ leapsuccess/src/main/java/com/leapsuccess/leapsuccess/App.java
@@ -0,0 +1,13 @@
+package com.leapsuccess.leapsuccess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
